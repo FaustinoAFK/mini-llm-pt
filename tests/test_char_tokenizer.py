9cf41312aee2076783df402d41824494f1fba8f4
FaustinoAFK <kelmar.faustino.j@gmail.com>
2026-05-25 19:15:25 -0400

    portal

--- tests/test_char_tokenizer.py
+++ tests/test_char_tokenizer.py
@@ -1,11 +1,5 @@
 from src.tokenizer.char_tokenizer import CharTokenizer
 
-
-def test_encode_decode_roundtrip():
-    text = "porta"
-    tokenizer = CharTokenizer(text)
-
-    ids = tokenizer.encode(text)
-    decoded = tokenizer.decode(ids)
-
-    assert decoded == text
+text = "portal"
+tokenizer = CharTokenizer(text)
+print(tokenizer.encode("portaria"))
